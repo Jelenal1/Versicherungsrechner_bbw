339f4076ec76822f2e17e5b954403a9f7140c043
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-04-21 15:01:36 +0200

    I am working on a basic landing page from which u can get to the different pages

--- calculator_app/src/App.jsx
+++ calculator_app/src/App.jsx
@@ -1,10 +1,12 @@
 import DamageCalulator from "./components/DamageCalculator";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route index path="/" element={<Landing />} />
         <Route path="/damagecalculator" element={<DamageCalulator />} />
       </Routes>
     </Router>
--- calculator_app/src/components/Landing.jsx
+++ calculator_app/src/components/Landing.jsx
@@ -1,5 +1,26 @@
+import Navbar from "./Navbar";
+
 function Landing() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="bg-gradient-to-r from-purple-600 to-blue-400 min-h-screen flex flex-col">
+        <h1 className="text-3xl mx-auto">Landing Page</h1>
+        <div className="p-5 flex gap-3 justify-center">
+          <div className="p-4 text-xl rounded-xl bg-[#ffffff3f] w-1/2 flex flex-col">
+            <h2 className="text-2xl text-center font-bold mb-2">
+              Schadensrechner
+            </h2>
+            Unser Schadensrechner berechnet zuverlässig die Schadenssumme,
+            welche die Versicherung zahlen wird. Wenn sie ihren Hauswert, bzw
+            Versicherungswert nicht wissen gibt es auch die möglichkeit diesen
+            Schritt für Schritt zu berechnen.
+            <button className="border-2">Go!</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
