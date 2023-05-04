e40102b9175ebcfe889469739744ab66c29a186b
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-05-04 16:10:40 +0200

    I updated the navbar and the routing

--- calculator_app/src/App.jsx
+++ calculator_app/src/App.jsx
@@ -1,6 +1,7 @@
 import DamageCalulator from "./components/DamageCalculator";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
+import InsuranceCalculator from "./components/InsuranceCalculator";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Landing />} />
         <Route path="/damagecalculator" element={<DamageCalulator />} />
+        <Route path="/insurancecalculator" element={<InsuranceCalculator />} />
       </Routes>
     </Router>
   );
--- calculator_app/src/components/Navbar.jsx
+++ calculator_app/src/components/Navbar.jsx
@@ -1,42 +1,53 @@
 import { BsFillCalculatorFill, BsHouseDoorFill } from "react-icons/bs";
-import { FaHouseDamage } from "react-icons/fa";
+import { FaCalculator, FaHouseDamage } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const styles = {
     spaceleft: `ml-5`,
     icons: `w-8 h-8`,
+    pills: `ml-2 flex items-center bg-violet-200 p-2 rounded-xl`,
+    pillstext: `ml-2`,
   };
 
   const navigate = useNavigate();
 
   return (
-    <div className="p-2 flex gap-2 h-20 bg-violet-300 border-b-2 border-black">
+    <div className="p-2 flex items-center gap-2 h-20 bg-violet-300 border-b-2 border-black">
       <div
-        className="flex items-center cursor-pointer"
+        className="flex items-center cursor-pointer border-t-2 border-b-2 border-black"
         onClick={() => {
           navigate("/");
         }}
       >
-        <h1 className="text-lg border-t-2 border-b-2 border-black">
-          Lorem Company
-        </h1>
+        <h1 className="text-lg my-2">Lorem Company</h1>
       </div>
       <button
-        className="ml-4"
+        className={styles.pills}
         onClick={() => {
           navigate("/");
         }}
       >
         <BsHouseDoorFill className={styles.icons} />
+        <h2 className={styles.pillstext}>Home</h2>
       </button>
       <button
-        className="ml-4"
+        className={styles.pills}
         onClick={() => {
           navigate("/damagecalculator");
         }}
       >
         <FaHouseDamage className={styles.icons} />
+        <h2 className={styles.pillstext}>Schadensrechner</h2>
+      </button>
+      <button
+        className={styles.pills}
+        onClick={() => {
+          navigate("/insurancecalculator");
+        }}
+      >
+        <FaCalculator className={styles.icons} />
+        <h2 className={styles.pillstext}>Versicherungssumme</h2>
       </button>
     </div>
   );
