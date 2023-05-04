0a20ca3bd4f03cbb999d867a9eb358f019b98ce6
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-05-04 22:22:15 +0200

    I am now beginning to implement the list of items that had been given

--- calculator_app/src/components/InsuranceCalculator.jsx
+++ calculator_app/src/components/InsuranceCalculator.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import InsuranceCalculatorInputForm from "./InsuranceCalculatorInputForm";
 import Navbar from "./Navbar";
 
 function InsuranceCalculator() {
+  const [houseItems, setHouseItems] = useState();
   return (
     <>
       <Navbar />
