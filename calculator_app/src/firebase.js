9df13d4b7609db809b6f7fd2c4f7bb7676854167
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-05-07 19:10:32 +0200

    I don't need to hide firbase keys

---
+++ calculator_app/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB1i-9Wqlxlga1UzzHxhaC_v8KJ2DioQ9c",
+    authDomain: "damagecalculator-prog-bbw.firebaseapp.com",
+    projectId: "damagecalculator-prog-bbw",
+    storageBucket: "damagecalculator-prog-bbw.appspot.com",
+    messagingSenderId: "227401220195",
+    appId: "1:227401220195:web:15e7e69a5639de02c7b856"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
